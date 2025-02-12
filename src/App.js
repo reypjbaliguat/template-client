f9f7d5eba68addc83bf76969e65671cad59dd105
reypjbaliguat <reypjbaliguat@gmail.com>
2025-02-12 20:13:05 +0800

    chore: repo fix - first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { client } from './apolloClient';
+import Login from './components/Login';
+import Dashboard from './pages/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import Register from './components/Register';
+
+function App() {
+    return (
+        <ApolloProvider client={client}>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route
+                        path="/dashboard"
+                        element={
+                            <PrivateRoute>
+                                <Dashboard />
+                            </PrivateRoute>
+                        }
+                    />
+                </Routes>
+            </Router>
+        </ApolloProvider>
+    );
+}
+
+export default App;
---
+++ src/components/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
+
+const LOGIN = gql`
+    mutation Login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+        }
+    }
+`;
+
+function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [login] = useMutation(LOGIN);
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const { data } = await login({ variables: { email, password } });
+            localStorage.setItem('token', data.login.token);
+            window.location.href = '/dashboard';
+        } catch (err) {
+            alert('Login failed');
+        }
+    };
+
+    return (
+        <form onSubmit={handleLogin}>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+        </form>
+    );
+}
+
+export default Login;
