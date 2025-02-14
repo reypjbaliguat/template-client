59a7e16e38b59a689b5d8eea1a7f4c7e1cf7530b
reypjbaliguat <reypjbaliguat@gmail.com>
2025-02-14 23:42:30 +0800

    feat(logout): added logout button to dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,16 @@
+import { Button, Grid2 } from '@mui/material';
 import React from 'react';
 
 function Dashboard() {
-    return <h1>Welcome to the Dashboard!</h1>;
+    const handleSignOut = () => {
+        localStorage.removeItem('token'); // Remove token
+        window.location.href = '/login'; // Redirect to login page
+    };
+    return (
+        <Grid2>
+            <Button onClick={() => handleSignOut()}> Logout </Button>
+        </Grid2>
+    );
 }
 
 export default Dashboard;
